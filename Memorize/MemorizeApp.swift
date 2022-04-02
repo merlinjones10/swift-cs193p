fb3c023d2555c184c83c134b25a0ed85447a8a42
merlinjones10 <78985319+merlinjones10@users.noreply.github.com>
2022-04-02 23:29:03 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Merlin Jones on 02/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
